359448c7b25578f2dc6780558790fad61525f961
wasimusu <wasim.khan@aggiemail.usu.edu>
2019-10-29 18:17:21 -0600

    Data iterator

--- datareader.py
+++ datareader.py
@@ -7,29 +7,51 @@ from utils import timeit
 
 
 class DataReader:
-    def __init__(self, fname):
+    def __init__(self, fname, batch_size=8):
         self.fname = fname
+        self.batch_size = batch_size
 
         if not os.path.exists(self.fname):
             raise ValueError("File {} does not exist".format(fname))
 
         # self.df = pd.DataFrame(pd.read_csv(self.fname, dtype={'AEP_MW': int}))
         self.df = pd.DataFrame(pd.read_csv(self.fname))
+        self.iter_count = 0
+
+        N = 25600
+        X = self.df[self.df.columns[0]]
+        self.Y = np.asarray(self.df[self.df.columns[1]][:N], dtype=np.float)
+
+        X = [repr_date(date) for date in X[:N]]
+        self.X = np.asarray(X)
+
+        self.num_batches = len(self.Y) // self.batch_size
 
     def get_data(self):
         # returns datetime and power usage.
         # have to represent datetime using lots of cosines/sines/plain values
         # cosines of day, month, hour,
-        N = 2000
-        X = self.df[self.df.columns[0]]
-        Y = np.asarray(self.df[self.df.columns[1]][:N], dtype=np.float)
+        return self.X, self.Y
 
-        X = [repr_date(date) for date in X[:N]]
-        X = np.asarray(X)
-        return X, Y
+    def __next__(self):
+        """ Outputs a batch of train and test data """
+        start = self.iter_count * self.batch_size
+        end = start + self.batch_size
+        self.iter_count += 1
+
+        # Start over again
+        if self.iter_count + 1 == self.num_batches:
+            self.iter_count = 0
+
+        return self.X[start:end], self.Y[start:end]
+
+    def __len__(self):
+        return self.num_batches
 
 
 if __name__ == '__main__':
     fname = "data/AEP_hourly.csv"
     datareader = DataReader(fname)
-    X, Y = datareader.get_data()
+    for i in range(len(datareader)):
+        X, Y = datareader.__next__()
+        print(i, X.shape, Y.shape)
--- models.py
+++ models.py
@@ -11,14 +11,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
 from networks import LSTMRegression, FCRegression
-
-
-def generate_data(N, sigma):
-    """ Generate data with given number of points N and sigma """
-    noise = np.random.normal(0, sigma, N)
-    X = np.random.uniform(0, 3, N)
-    Y = 2 * X ** 2 + 3 * X + 1 + noise  # arbitrary function
-    return X, Y
+from datareader import DataReader
 
 
 class Model:
@@ -30,7 +23,7 @@ class Model:
         self.bidirectional = bidirectional
         self.num_layers = num_layers
 
-    def train(self):
+    def train(self, data_iter):
         model = LSTMRegression(self.input_dim,
                                self.hidden_dim,
                                output_dim=1,
@@ -38,39 +31,54 @@ class Model:
                                num_layers=self.num_layers,
                                bidiectional=self.bidirectional)
 
-        model = FCRegression(self.input_dim, self.batch_size)
+        # model = FCRegression(self.input_dim, self.batch_size)
         print("Model : ", model)
         criterion = nn.MSELoss()
         optimizer = optim.SGD(params=model.parameters(), lr=self.lr)
 
         # Training parameters
-        num_epoch = 1000
+        num_epoch = 100
 
         for epoch in range(num_epoch):
-            inputs, labels = generate_data(N=batch_size, sigma=0)
-            inputs = torch.tensor(inputs).reshape(-1, 1).float().unsqueeze(0)
+            epoch_loss = 0
+            for i in range(len(data_iter)):
+                inputs, labels = data_iter.__next__()
+
+                inputs = torch.tensor(inputs).reshape(1, self.batch_size, -1).float()
+                labels = torch.tensor(labels).reshape(-1, 1).float()
+                output = model(inputs)
+
+                model.zero_grad()
+                loss = criterion(output, labels)
+                loss.backward(retain_graph=True)
+                optimizer.step()
+
+                epoch_loss += loss
+                if i % 100 == 0:
+                    self.compute_loss(dataiter=data_iter, model=model, criterion=criterion)
+                    break
+
+    def compute_loss(self, dataiter, model, criterion):
+        epoch_loss = 0
+        for j in range(len(dataiter)):
+            inputs, labels = dataiter.__next__()
+            inputs = torch.tensor(inputs).reshape(1, self.batch_size, -1).float()
             labels = torch.tensor(labels).reshape(-1, 1).float()
 
-            output = model(inputs)
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            epoch_loss += loss.item()
+            if j == 9: break
 
-            model.zero_grad()
-            loss = criterion(output, labels)
-            loss.backward(retain_graph=True)
-            optimizer.step()
+        print("Epoch Loss : {}".format("%.2f" % epoch_loss))
 
-            if epoch % 100 == 0:
-                print("Epoch  {}\tLoss : {}".format(epoch, "%.2f" % loss.item()))
-
-                # Test the model
-                input, labels = generate_data(N=batch_size, sigma=0)
-                inputs = torch.tensor(input).reshape(-1, 1).float().unsqueeze(0)
-
-                with torch.no_grad():
-                    outputs = model(inputs)
-                    inputs = torch.tensor([input]).reshape(-1, 1).float().unsqueeze(0)
-                    outputs = np.round(outputs.view(1, -1).detach().numpy(), 2)
-                    print(np.round(input, 2), '\n',
-                          np.round(labels, 2), '\n', outputs, '\n\n')
+        inputs, labels = dataiter.__next__()
+        inputs = torch.tensor(inputs).reshape(1, self.batch_size, -1).float()
+        labels = torch.tensor(labels).reshape(-1, 1).float()
+        with torch.no_grad():
+            outputs = model(inputs)
+            outputs = np.round(outputs.view(1, -1).detach().numpy(), 2)
+            print(np.round(labels, 2), '\n', outputs, '\n\n')
 
     def predict(self):
         pass
@@ -79,10 +87,10 @@ class Model:
 if __name__ == '__main__':
     # Parameters of the model
     # You can change any of the parameters and expect the network to run without error
-    input_dim = 1
+    input_dim = 4
     num_layers = 1
     bidirectional = False
-    hidden_dim = 512
+    hidden_dim = 128  # 512 worked best so far
     batch_size = 8
 
     model = Model(input_dim=input_dim,
@@ -90,5 +98,11 @@ if __name__ == '__main__':
                   bidirectional=bidirectional,
                   hidden_dim=hidden_dim,
                   batch_size=batch_size,
-                  lr=0.005)
-    model.train()
+                  lr=0.01)
+
+    fname = "data/AEP_hourly.csv"
+    datareader = DataReader(fname, batch_size=batch_size)
+    for i in range(len(datareader)):
+        X, Y = datareader.__next__()
+
+    model.train(datareader)
